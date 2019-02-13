b7baabaa113038e6cfd6e2f447c208223541824b
Isaac Brito <ibritomorales@gmail.com>
2019-02-13 19:10:10 +1000

    new based on first

---
+++ scripts/second.R
@@ -0,0 +1,13 @@
+test <- posthoc(path = "Scenario1", outdir = "scripts/", scenario = "1")
+
+
+folders <- paste("Scenario", seq(1, 20, 1), sep = "")
+
+ls_csv <- list()
+for (j in 1:length(sc_files)) {
+  
+  ls_csv[[j]] <- test <- posthoc(path = folders[i], outdir = "CSV/", scenario = i)  
+  
+}
+
+# do a parallel on this function
