3472cb24bf0b098d7186a768d880dc2f706118d0
Isaac Brito <ibritomorales@gmail.com>
2020-01-13 13:09:30 +1000

    final commit tested and working

--- scripts/PosthocMarxan_01_IterationsScenarios.R
+++ scripts/PosthocMarxan_01_IterationsScenarios.R
@@ -7,7 +7,7 @@
 # path: 
 
 
-posthoc_marxan <- function(path) { 
+posthoc_marxan <- function(path, outdir) { 
   
   # List of pacakges that we will use
     list.of.packages <- c("raster", "sf", "dplyr", "tidyr", "readr", "lwgeom", "doParallel", "parallel")
@@ -79,92 +79,18 @@ posthoc_marxan <- function(path) {
                  perimeter_m = perimeter,
                  per_circle_m = per_circle,
                  fragmentation_m = fragmentation)
-    # Final list objects
+    # Final list object
       scenario_list[[i]] <- geom_list
       }
-      
-  # 
-  
-      
+  # Final dataframe for all iterations-scenarios in directory
+    iteration_df <- do.call(rbind, scenario_list)
+    name.df <- paste("PostHoc_Calibration")
+    write.csv(iteration_df, paste(outdir, name.df, ".csv", sep = ""), row.names = FALSE)  
   
 }
 
-
-
-
-path <- "Iterations"
-# Define all the directories 
-dir.scenarios <- list.dirs(paste(list.dirs(path = path, full.names = TRUE, recursive = FALSE)), full.names = TRUE, recursive = FALSE)
-
-
-scenario_list <- list()
-
-for(i in 1:length(dir.scenarios)) {
-  
-  # Define folders per scenario
-  folders <- list.dirs(dir.scenarios[i], full.names = TRUE, recursive = FALSE)
-  # Files location
-  out_files <- list.files(path = folders[2], pattern = "*_r.*.csv$", full.names = TRUE)
-  out_log <- list.files(path = folders[2], pattern = "*_log.*.dat$", full.names = TRUE) 
-  shp_files <- list.files(path = folders[3], pattern = "*.shp$", full.names = TRUE)
-  # Read shapefile just one time 
-  dt_shp <- st_read(shp_files) %>% dplyr::select(id, cost, geometry) %>% arrange(id)
-  
-  # Define the parallel structure
-  UseCores <- detectCores() -1
-  cl <- makeCluster(UseCores)  
-  registerDoParallel(cl)
-  
-  ls_geom <- list() # empty list to allocate results
-  
-  geom_list <- foreach(j = 1:length(out_files), .packages = c("sf", "raster", "dplyr", "tidyr", "readr", "lwgeom")) %dopar% {
-    
-    dt <- read.table(out_files[j], sep = ",", header = TRUE) # read every element from the out_files object
-    dt <- dt[(dt[,2] == 1),] %>% rename(id = names(dt[1]), solution = names(dt[2]))
-    # Marxan's solution names
-    name <- unlist(strsplit(out_files[j], "_"))[2]
-    name <- sub(pattern = "*.csv", "", name)
-    # Best solution find 
-    dt_outlog <- readLines(out_log)
-    name_outlog <- dt_outlog[grep("Best solution*", x = dt_outlog)]
-    name_outlog <- unlist(strsplit(name_outlog, " "))[5]
-    # Extract from the shapefile the object dt
-    dt2 <- dt_shp[dt_shp$id %in% dt$id,]
-    head(dt2)
-    dt3 <- dt2 %>% dplyr::summarise(new_cost = sum(cost, do_union = TRUE))
-    dt_final <- dt3 %>% mutate(area = st_area(dt3), 
-                               perimeter = st_perimeter(dt3), 
-                               solution = name,
-                               iteration = readr::parse_number(unlist(strsplit(folders[1], split = "/"))[[2]]),
-                               scenario = readr::parse_number(unlist(strsplit(folders[1], split = "/"))[[3]]),
-                               best_solution = ifelse(as.character(readr::parse_number(name)) == name_outlog, "YES", "NO"))
-    # Fragmentation process
-    dt_final <- dt_final %>% mutate(per_circle = (sqrt(area/pi))*2*pi)
-    dt_final <- dt_final %>% mutate(fragmentation = perimeter/per_circle)
-    
-    ls_geom[[j]] <- dt_final # a list where results will be added
-  } 
-  stopCluster(cl)
-  
-  geom_list <- do.call(rbind, geom_list)
-  
-  geom_list <- geom_list %>% data.frame %>% dplyr::select(-geometry) %>% 
-    mutate(area = as.numeric(area), 
-           perimeter = as.numeric(perimeter),
-           per_circle = as.numeric(per_circle),
-           fragmentation = as.numeric(fragmentation)) %>% 
-    rename(area_m2 = area,
-           perimeter_m = perimeter,
-           per_circle_m = per_circle,
-           fragmentation_m = fragmentation)
-  
-  # tail(geom_list, 12)
-  
-  scenario_list[[i]] <- geom_list
-}
-
-length(scenario_list)
-head(scenario_list[[11]], 3)
+  system.time(posthoc_marxan(path = "Iterations",
+                             outdir = "Iterations/"))
 
 
 
