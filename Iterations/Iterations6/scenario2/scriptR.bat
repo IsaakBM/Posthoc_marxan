afdd9f911bea4017a94b2e27595fe67c05284345
Isaac Brito <ibritomorales@gmail.com>
2020-06-04 15:26:14 +1000

    deleting old files

--- Iterations/Iterations6/scenario2/scriptR.bat
+++
@@ -1,1 +0,0 @@
-type "C:\Users\User\Desktop\Connectivity\Connectivity\Scenarios\Scenario BLM3\script.R" | "C:\Program Files\R\R-3.5.0\bin\R.exe" --slave --vanilla
--- Iterations/Iterations6/scenario7/scriptR.bat
+++
@@ -1,1 +0,0 @@
-type "C:\Users\User\Desktop\Connectivity\Connectivity\Scenarios\ScenarioBLM03.2\script.R" | "C:\Program Files\R\R-3.5.0\bin\R.exe" --slave --vanilla
